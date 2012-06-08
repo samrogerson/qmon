da16958fb8b7d1c34279142e531d5d84e52eb5ec
Sam Rogerson <samr@cern.ch>
2012-06-08 16:51:51 +0100

    null behavior implemented

--- queuetools.py
+++ queuetools.py
@@ -1,35 +1,40 @@
+from commands import getoutput
+
+def separate(output):
+    return output.split()[1:]
+
+def combine(output):
+    return 0, 0, 0
 
 class QueueJobAnalyzer(object):
-    def __init__():
-        self.output_format = "{r:>8} {q:>8} {o:>8}"
+    def __init__(self):
+        self.pl = 35
+        self.output_format = "{u:<8} {r:>8} {q:>8} {o:>8}"
         self.user_job_command = None
         self.user_job_postprocess = None
 
     def get_user_job_counts(self,user):
-        status_output = self.user_job_command(user)
+        status_output = getoutput(self.user_job_command.format(user=user))
         return self.user_job_postprocess(status_output)
 
     def print_user_job_count(self, user):
         running, queued, other = self.get_user_job_counts(user)
-        print "="*26
-        print self.output_format(r=running, q=queued, o=other)
-        print "="*26
-
+        print "="*self.pl
+        print self.output_format.format(u=user, r=running, q=queued, o=other)
+        print "="*self.pl
 
 class SGEJobAnalyzer(QueueJobAnalyzer):
-    def __init__():
-        super(SGEJobAnalyser,self).__init__()
+    def __init__(self):
+        super(SGEJobAnalyzer,self).__init__()
         self.user_job_command = "qstat -u {user}"
-        self.user_job_postprocess = self.combine
-
-    def combine(output):
-        return 0, 0, 0
+        self.user_job_postprocess = combine
 
 class CondorJobAnalyzer(QueueJobAnalyzer):
-    def __init__():
-        super(CondorJobAnalyser,self).__init__()
+    def __init__(self):
+        super(CondorJobAnalyzer,self).__init__()
         self.user_job_command = "condor_status -submitter | grep ' {user}'"
-        self.user_job_postprocess = self.separate
+        self.user_job_postprocess = separate
 
-    def separate(output):
-        return output.split()[1:]
+if __name__=="__main__":
+    test_analyzer = SGEJobAnalyzer()
+    test_analyzer.print_user_job_count("sr505")
