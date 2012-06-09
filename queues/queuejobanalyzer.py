598861c395dae82c4ead1f4a75f948f81576de20
Sam Rogerson <samr@cern.ch>
2012-06-09 13:45:17 +0100

    start doing full user breakdown

--- queues/queuejobanalyzer.py
+++ queues/queuejobanalyzer.py
@@ -4,10 +4,10 @@ class QueueJobAnalyzer(object):
     def __init__(self):
         pass
 
-    def _get_user_job_string(self, user):
-        return getoutput(self.user_job_command.format(u=user))
+    def _get_queue_job_string(self, queue):
+        return getoutput(self.queue_job_command.format(q=queue))
 
-    def _user_job_postprocess(self, user):
+    def _queue_job_postprocess(self, queue):
         return None, None, None
 
     def _get_queue_status_string(self):
@@ -17,10 +17,15 @@ class QueueJobAnalyzer(object):
         return None
 
     # interfaces
-    def get_user_job_counts(self,user):
-        status_output = self._get_user_job_string(user)
-        return self._user_job_postprocess(status_output)
+    def get_queue_job_counts(self, queues = None):
+        if queues == None:
+            queues = self.queues
+        q_dict = {}
+        for q in queues :
+            status_output = self._get_queue_job_string(q)
+            q_dict[q] = self._queue_job_postprocess(status_output)
+        return q_dict
 
-    def get_queue_status(self, queues=None):
+    def get_queue_status(self):
         status_output = self._get_queue_status_string()
         return self._queue_status_postprocess(status_output)
