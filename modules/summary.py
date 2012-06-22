8f810f057744055a580453b262fade745fdaf70e
Sam Rogerson <samr@cern.ch>
2012-06-22 11:29:10 +0100

    use print as a function and allow chaning of output method

--- modules/summary.py
+++ modules/summary.py
@@ -1,11 +1,13 @@
+from __future__ import print_function
+
 from collections import defaultdict
 
 from queues.sgejobanalyzer import SGEJobAnalyzer
 from queues.condorjobanalyzer import CondorJobAnalyzer
 
-def full_summary(QAnalyzer):
-    per_queue(QAnalyzer)
-    per_user(QAnalyzer, title=True)
+def full_summary(QAnalyzer,outfunc=print, out_end=""):
+    per_queue(QAnalyzer, outfunc=outfunc)
+    per_user(QAnalyzer, title=True, outfunc=outfunc)
 
 def user_status_from_queue_status(d):
     # get our unique list of users
@@ -21,7 +23,7 @@ def user_status_from_queue_status(d):
             user_statuses[user][q] = status_dict
     return user_statuses
 
-def per_user(analyzer, title=False):
+def per_user(analyzer, title=False, outfunc=print):
     output_format = "{u:<10}"
     per_queue_format = "[{r:>3},{q:>4},{o:>4}] "
     queue_title_format = "{q:<16}"
@@ -37,12 +39,12 @@ def per_user(analyzer, title=False):
         banner += queue_banner_base
         sub_banner += queue_sub_banner_base
 
-    print banner
+    outfunc(banner)
     queue_title_str = output_format.format(u="User")
     all_titles = [ queue_title_format.format(q=q) for q in analyzer.queues ]
     queue_title_str += "".join(all_titles)
-    print queue_title_str
-    print sub_banner
+    outfunc(queue_title_str)
+    outfunc(sub_banner)
 
     q_dict = analyzer.get_queue_job_counts()
     user_status = user_status_from_queue_status(q_dict)
@@ -55,12 +57,12 @@ def per_user(analyzer, title=False):
             q = job_info.get(queue,default_statuses).get('qw',0)
             o = job_info.get(queue,default_statuses).get('o',0)
             u_string += per_queue_format.format(r=r, q=q, o=o)
-        print u_string
+        outfunc(u_string)
 
     if title :
-        print banner
+        outfunc(banner)
 
-def per_queue(analyzer, output_format = None) :
+def per_queue(analyzer, output_format=None, outfunc=print) :
     output_format = "{q:<12} {a:>8} {t:>8}"
     banner_format = "{:=^12}={:=^8}={:=^8}"
     sub_banner_format = "{:-^12}-{:-^8}-{:-^8}"
@@ -68,11 +70,11 @@ def per_queue(analyzer, output_format = None) :
     sub_banner = sub_banner_format.format("","","")
 
     q_dict = analyzer.get_queue_status()
-    print banner
-    print output_format.format(q="Queue", a="Free", t="Total")
-    print sub_banner
+    outfunc(banner)
+    outfunc(output_format.format(q="Queue", a="Free", t="Total"))
+    outfunc(sub_banner)
 
     for q, cores in q_dict.iteritems() :
-        print output_format.format(q=q, a=cores['a'], t=cores['t'])
+        outfunc(output_format.format(q=q, a=cores['a'], t=cores['t']))
 
-    print banner
+    outfunc(banner)
